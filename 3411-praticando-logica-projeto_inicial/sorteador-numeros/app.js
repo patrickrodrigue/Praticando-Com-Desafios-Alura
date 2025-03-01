2c3007baec7cd792bb09eb189f2716ccaad37eea
Patrick Rodrigues <patrickrodrigued1239@gmail.com>
2025-03-01 12:12:35 -0300

    Ativando Sorteador de Números

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,12 +1,31 @@
-let quantidadeNumero; ; 
-
 function sortear() {
-    quantidadeNumero = document.getElementById('quantidade').value;
-    if(!quantidadeNumero){
-        exibirMensagemAviso('label');
-        let pedido = 'Por favor, insira um número';
-        
+    let minQuantidade = 1;
+    let min = document.getElementById('de').value;
+    let max = document.getElementById('ate').value;
+    let numerosSorteados = [];
+    let quantidadeNumero = document.getElementById('quantidade').value;
+    if (!quantidadeNumero) {
+        exibirMensagemAviso('aviso', 'Por favor, insira um número');
+    } else {
+        while (minQuantidade <= quantidadeNumero) {
+            let numAleatorio = parseInt(Math.random() * (max - min) + min);
+            numerosSorteados.push(numAleatorio);
+            minQuantidade++
+            exibirMensagemAviso('aviso', `Números sorteados: ${numerosSorteados}`);
+        }
     }
+
+}
+function exibirMensagemAviso(elementId, mensagem) {
+    document.getElementById(elementId).innerText = mensagem;
 }
 
- 
+function limpaCampo(){
+    max.value = '';
+    min.value = '';
+    numerosSorteados = '';
+}
+// function reiniciar(){
+//     limpaCampo();
+//     document.getElementById('btn-reiniciar').setAttribute('disabled', true);
+// }
