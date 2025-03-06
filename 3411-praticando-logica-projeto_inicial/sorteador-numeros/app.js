88a629e57c92579081f7a4890f79868b614ed7ff
Patrick Rodrigues <patrickrodrigued1239@gmail.com>
2025-03-06 11:49:24 -0300

    Ativando_btn_reiniciar

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -4,6 +4,7 @@ function sortear() {
     let max = document.getElementById('ate').value;
     let numerosSorteados = [];
     let quantidadeNumero = document.getElementById('quantidade').value;
+
     if (!quantidadeNumero) {
         exibirMensagemAviso('aviso', 'Por favor, insira um número');
     } else {
@@ -12,20 +13,32 @@ function sortear() {
             numerosSorteados.push(numAleatorio);
             minQuantidade++
             exibirMensagemAviso('aviso', `Números sorteados: ${numerosSorteados}`);
+            alterarStatusbtn();
         }
     }
+
 
 }
 function exibirMensagemAviso(elementId, mensagem) {
     document.getElementById(elementId).innerText = mensagem;
 }
 
-function limpaCampo(){
-    max.value = '';
-    min.value = '';
-    numerosSorteados = '';
+function alterarStatusbtn(){
+    let btn = document.getElementById('btn-reiniciar');
+    if(btn.classList.contains('container__botao-desabilitado')){
+        btn.classList.remove('container__botao-desabilitado');
+        btn.classList.add('container__botao');
+    }else {
+        btn.classList.remove('container__botao');
+        btn.classList.add('container__botao-desabilitado');
+    }
 }
-// function reiniciar(){
-//     limpaCampo();
-//     document.getElementById('btn-reiniciar').setAttribute('disabled', true);
-// }
+
+function reiniciar(){
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('quantidade').value =  '';
+    document.getElementById('aviso').innerHTML = '<label id="aviso" class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    
+    alterarStatusbtn()
+}
